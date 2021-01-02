164278300413a5121c02c06d2e2648f569f85715
melih <melih.korkmaz@fortanix.com>
2021-01-02 08:47:46 +0100

    s3 upload test

--- functions/helloWorld.js
+++ functions/helloWorld.js
@@ -1,24 +1,83 @@
 const AWS = require("aws-sdk");
+var Multipart = require('lambda-multipart');
+
 
 const s3 = new AWS.S3({
   accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
 });
 
-exports.handler = async (event, context, callback) => {
-  const params = {
+const parseMultipartFormData = async event => {
+  return new Promise((resolve, reject) => {
+    const parser = new Multipart(event);
+
+    parser.on("finish", result => {
+      resolve({ fields: result.fields, files: result.files });
+    });
+
+    parser.on("error", error => {
+      return reject(error);
+    });
+  });
+};
+
+const getFileExtension = file => {
+  const headers = file["headers"];
+  if (headers == null) {
+    throw new Error(`Missing "headers" from request`);
+  }
+
+  const contentType = headers["content-type"];
+  if (contentType == "image/jpeg") {
+    return "jpg";
+  }
+
+  throw new Error(`Unsupported content type "${contentType}".`);
+};
+
+const uploadFileIntoS3 = async file => {
+  const ext = getFileExtension(file);
+  const options = {
     Bucket: "docmaker-templates",
-    Key: `name-of-your-file.json`,
-    Body: JSON.stringify({ hello: "world" }),
-    ACL: "private",
-    ContentEncoding: "utf8",
-    ContentType: `application/json`,
+    Key: `${uuidv4()}.${ext}`,
+    Body: file
   };
 
   try {
-    const result = await s3.upload(params).promise();
-    return { statusCode: 200, body: JSON.stringify(result) }
+    await s3.upload(options).promise();
   } catch (err) {
-    return { statusCode: 500, body: err.message }
+    console.error(err);
+    throw err;
   }
+};
+
+exports.handler = async (event, context, callback) => {
+  const { fields, files } = await parseMultipartFormData(event);
+  await Promise.all(
+    files.map(async file => {
+      await uploadFileIntoS3(file);
+    })
+  );
+
+  return {
+    successCode: 200,
+    body: JSON.stringify({ msg: 'OK' })
+  }
+  // const params = {
+  //   Bucket: "docmaker-templates",
+  //   Key: `name-of-your-file.json`,
+  //   Body: JSON.stringify({ hello: "world" }),
+  //   ACL: "private",
+  //   ContentEncoding: "utf8",
+  //   ContentType: `application/json`,
+  // };
+
+  // try {
+  //   const result = await s3.upload(params).promise();
+  //   return { statusCode: 200, body: JSON.stringify(result) }
+  // } catch (err) {
+  //   return { statusCode: 500, body: err.message }
+  // }
+
+
 }
--- src/modules/dashboard/Dashboard.tsx
+++ src/modules/dashboard/Dashboard.tsx
@@ -1,11 +1,36 @@
+import axios from 'axios';
 import { Button } from 'evergreen-ui';
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
 import { userOperations, userSelectors } from '../../store/user';
+
 
 const Dashboard = () => {
   const dispatch = useDispatch();
+  const [file, setFile] = useState<File | undefined>();
   const user = useSelector(userSelectors.getUser);
+  
+  
+  const onFileChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    if (evt.target.files && evt.target.files.length) {
+      setFile(evt.target.files[0]);
+    }
+  };
+  
+  const onFileUpload = async () => {
+    if (!file) {
+      return;
+    }
+    
+    const formData = new FormData(); 
+    formData.append( 
+      'myFile', 
+      file, 
+      file?.name
+    ); 
+    const res = await axios.post("/.netlify/functions/helloWorld", formData);
+    console.log('res', res);
+  };
   
   const handleLogout = () => {
     dispatch(userOperations.logout());  
@@ -22,6 +47,12 @@ const Dashboard = () => {
         Logout
       </Button>
     </div>
+    <div> 
+        <input type="file" onChange={onFileChange} /> 
+        <button onClick={onFileUpload}> 
+          Upload! 
+        </button> 
+    </div> 
   </div>)
 };
 
